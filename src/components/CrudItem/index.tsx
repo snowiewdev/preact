b5b5e57fb4d96174c83106912254a4a35d7a81af
snowie <snowiewongdev@gmail.com>
2022-08-15 17:12:10 -0700

    counter testing

--- src/components/CrudItem/index.tsx
+++ src/components/CrudItem/index.tsx
@@ -1,5 +1,16 @@
 import { FunctionalComponent, h, Fragment }  from 'preact'
 
+// import * as style from './style.scss';
+
+type Props = {
+  text: string;
+  id: string;
+  isCompleted: boolean;
+  OnEditTodo: (id: string) => void;
+  OnDeleteTodo: (id: string) => void;
+  onToggleCompleted: (id: string) => void;
+}
+
 const index = () => {
   return (
     <div>index</div>
--- src/components/testing/testing.js
+++ src/components/testing/testing.js
@@ -1,9 +1,39 @@
-import React from 'react'
-import styles from './style.css';
+import { h } from 'preact';
+import { useState } from 'preact/hooks';
+
+// import * as S from './styles';
 
 const Testing = () => {
+
+  const [name, setName] = useState('Mary');
+
+  const [count, setCount] = useState(0);
+  const increment = (e) => {
+    e.preventDefault();
+    setCount(count+1);
+  } 
+  const decrement = (e) =>{
+    e.preventDefault();
+    setCount(count-1);
+  } 
+  const reset = (e) => {
+    e.preventDefault();
+    setCount(0);
+  };
+   
+
   return (
-    <div className={styles.red}>Testing</div>
+    <>
+      <div>Testing</div>
+        {/* <input type="text" name="name" onChange={}/> */}
+        <p> Counter Count: {count} </p>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+      <form> 
+        <button type="submit">Submit</button>
+      </form>
+    </>
   )
 }
 
